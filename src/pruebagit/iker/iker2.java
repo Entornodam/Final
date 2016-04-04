c40d49b314dd45c5a29bce10c45f2e52b1deaf2b
Vespertino B <Vespertino B@13-C5>
2016-04-04 16:53:21 +0100

    comit 2

---
+++ src/pruebagit/iker/iker2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebagit.iker;
+
+/**
+ *
+ * @author Vespertino B
+ */
+public class iker2 {
+    public iker2()
+    {   System.out.println("hola mundo");
+    }
+}
