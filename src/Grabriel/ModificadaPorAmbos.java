deae9760bba90a08c385d349255ad38cfc96c004
Vespertino <Vespertino@13-C5>
2016-04-08 16:21:30 +0100

    nueva clase ambos

---
+++ src/Grabriel/ModificadaPorAmbos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Grabriel;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class ModificadaPorAmbos {
+    private int x;
+    
+ public ModificadaPorAmbos(){
+  x=10;  
+}
+}
+    
+
